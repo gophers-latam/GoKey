7027f22c49ba0b3adf67b8488f7cca466255c6a6
tomas lingotti <tomaslingotti@gmail.com>
2021-04-25 14:04:34 -0300

    some renaming and adding comments.

--- aplication/Interfaces/operations.go
+++
@@ -1,8 +0,0 @@
-package Aplication
-
-type Operations interface {
-	get(key string) (string, error)
-	set(key string, value string) (bool, error)
-	upsert(key string, value string) (bool, error)
-	delete(key string) (bool, error)
-}
