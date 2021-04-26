d503f1c0ada33ba11a21acf3061607a01f01d2c6
Brandon Bermudez <44240580+brandonjoker17@users.noreply.github.com>
2021-04-26 06:16:11 -0600

    feat: adding key hasher algorithm (#4)
    * feat: adding key hasher algorithm
    
    * fix(key-hasher): requested changes added
    
    * fix(key-hasher):
    changing the type of argument in the hash generator function

---
+++ gokey/hash.go
@@ -0,0 +1,14 @@
+package gokey
+
+import (
+	"crypto/md5"
+	"encoding/hex"
+)
+
+//generateMD5HashFromKey is a hash generator function according to input(key)
+//using md5 algorith
+func generateMD5HashFromKey(key []byte) string {
+	algorithm := md5.New()
+	algorithm.Write(key)
+	return hex.EncodeToString(algorithm.Sum(nil))
+}
