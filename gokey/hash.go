7c9110c11f5690b7e1da4c7c5ddbe79976a5d4bf
Brandon Bermudez <brandonmorales555@gmail.com>
2021-04-25 22:11:34 -0600

    fix(key-hasher): changing the type of argument in the hash generator function

--- gokey/hash.go
+++ gokey/hash.go
@@ -7,8 +7,8 @@ import (
 
 //generateMD5HashFromKey is a hash generator function according to input(key)
 //using md5 algorith
-func generateMD5HashFromKey(key string) string {
+func generateMD5HashFromKey(key []byte) string {
 	algorithm := md5.New()
-	algorithm.Write([]byte(key))
+	algorithm.Write(key)
 	return hex.EncodeToString(algorithm.Sum(nil))
 }
