9a9496f0fb01e94fae5b5d39cfd8dcfd1f5ecb88
Martin <martinald467@gmail.com>
2021-05-07 18:32:04 +0200

    docs: capital letters at the beginning of sentences and dots at the end

--- gokey/operations.go
+++ gokey/operations.go
@@ -4,15 +4,15 @@ import "time"
 
 // Operations contains all the basic operations for all the interactions with the data structure (cache).
 type Operations interface {
-	// get returns a value and an optional error given a key.
+	// Get returns a value and an optional error given a key.
 	Get(key string) ([]byte, error)
 
-	// upsert is for create/update operation in the cache.
+	// Upsert is for create/update operation in the cache.
 	// ttl must be positive. If ttl is equal to 0, the entry will not expire.
-	// Returns whether the entry was created with this operation or not (updated) and an optional error
+	// Returns whether the entry was created with this operation or not (updated) and an optional error.
 	Upsert(key string, value []byte, ttl time.Duration) (bool, error)
 
-	// delete removes a value given a key.
-	// Returns whether the entry was deleted or not and an optional error
+	// Delete removes a value given a key.
+	// Returns whether the entry was deleted or not and an optional error.
 	Delete(key string) (bool, error)
 }
