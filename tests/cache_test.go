ff928517d3ff4fbce6859b3c61acd1e568090ad7
Ezequiel Reyna <ezequiel.reyna@gmail.com>
2021-05-12 12:38:42 -0300

    add expired assertion in Get function, refactor Upsert and fix tests (#13)
    * add expired assertion in Get function, refactor Upsert and fix tests
    
    * add one more Get call to the test to confirm the deletion was ok
    
    * add some more refactor to tests and operations interface comments. Capture the -1 possibility for ttl for unexpired keys
    
    * return only one error message for non-existing keys or expired

--- tests/cache_test.go
+++ tests/cache_test.go
@@ -1,7 +1,9 @@
 package tests
 
 import (
+	"errors"
 	"testing"
+	"time"
 
 	"github.com/gophers-latam/GoKey/gokey"
 )
@@ -12,29 +14,55 @@ var operations gokey.Operations = new(gokey.Cache)
 func TestCacheUpsert(t *testing.T) {
 	_, err := operations.Upsert("key", []byte("value"), -1)
 	if err != nil {
-		t.Error(err.Error())
+		t.Error("key cannot be empty")
 	}
 
 	_, err = operations.Upsert("", []byte("value"), 1)
-	if err != nil {
+	if err == nil {
 		t.Error(err.Error())
 	}
 }
 
 // go test -run TestCacheGet -v
 func TestCacheGet(t *testing.T) {
-	_, err := operations.Upsert("key", []byte("value"), 10)
+	_, err := operations.Upsert("key", []byte("value"), 10*time.Second)
 	if err != nil {
-		t.Error("Expected no errors in Upsert method, got:", err.Error())
+		t.Error("expected no errors in Upsert method, got:", err.Error())
 	}
 
 	value, err := operations.Get("key")
 	if err != nil {
-		t.Error("Expected no errors in Get method, got:", err.Error())
+		t.Error("expected no errors in Get method, got:", err.Error())
 	}
 
-	if value != nil {
-		t.Error("Expected a value, got nil")
+	if value == nil {
+		t.Error("expected a value, got nil")
+	}
+}
+
+// go test -run TestCacheGetExpiredKey -v
+func TestCacheGetExpiredKey(t *testing.T) {
+	_, err := operations.Upsert("key", []byte("value"), 1*time.Second)
+	if err != nil {
+		t.Error("expected no errors in Upsert method, got:", err.Error())
+	}
+	time.Sleep(1 * time.Second)
+
+	_, err = operations.Get("key")
+	if err == nil {
+		t.Error("expected ErrExpiredKey, got: nil")
+	}
+	if err != nil {
+		if !errors.Is(err, gokey.ErrNoExistKey) {
+			t.Error("expected ErrExpiredKey, got:", err.Error())
+		}
+	}
+
+	_, err = operations.Get("key")
+	if err != nil {
+		if !errors.Is(err, gokey.ErrNoExistKey) {
+			t.Error("expected 'key does not exist' error message, got:", err.Error())
+		}
 	}
 }
 
@@ -42,20 +70,49 @@ func TestCacheGet(t *testing.T) {
 func TestCacheGetEmptyKey(t *testing.T) {
 	_, err := operations.Get("")
 	if err == nil {
-		t.Error("Expected empty key error message, got nil")
+		t.Error("expected empty key error message, got nil")
 	}
 }
 
-// go test -run TestCacheGetUnknowKey -v
-func TestCacheGetUnknowKey(t *testing.T) {
+// go test -run TestCacheGetUnknownKey -v
+func TestCacheGetUnknownKey(t *testing.T) {
 	_, err := operations.Get("Key")
 	if err == nil {
-		t.Error("Expected 'no related values' error message, got nil")
+		t.Error("expected 'no related values' error message, got nil")
 	}
 
 	// case ok
 	_, err = operations.Upsert("key", []byte("value"), 0)
 	if err != nil {
 		t.Error("here: unexpected error")
+	}
+}
+
+// go test -run TestUpsertSameKey -v
+func TestUpsertSameKey(t *testing.T) {
+	key := "key"
+	value := "value"
+	newValue := "newValue"
+	_, err := operations.Upsert(key, []byte(value), -1)
+
+	if err != nil {
+		t.Error("err should be nil", err)
+	}
+
+	v, err := operations.Get(key)
+
+	if err != nil {
+		t.Error("err should be nil", err)
+	}
+
+	if string(v) != value {
+		t.Errorf("got different value from cache. Expected: %s, got: %s", value, string(v))
+	}
+
+	_, err = operations.Upsert(key, []byte(newValue), -1)
+	v, err = operations.Get(key)
+
+	if string(v) != newValue {
+		t.Errorf("got different value from cache. Expected: %s, got: %s", newValue, string(v))
 	}
 }
